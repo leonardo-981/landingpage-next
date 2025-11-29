49c12b89c6d8b5ff02f29a4ef164d0ef44b30176
charset404 <sobrelevel@gmail.com>
2025-11-28 23:40:09 -0300

    Fix: footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,10 +2,10 @@
 
 export function Footer() {
   return (
-    <footer
-      className={` w-full text-center py-4 bg-neutral-900 text-white`}
-    >
-      <p>Copyright &copy; 2025 Minha Empresa. Todos os direitos reservados.</p>
+    <footer className={` w-full text-center py-4 bg-neutral-900 text-white`}>
+      <p>
+        Copyright &copy; 2025 Movimentos Mágicos. Todos os direitos reservados.
+      </p>
     </footer>
   );
 }
